efb30fa9e36b98768f36d5cbd0e251fe4a71bc1a
Maleesha Indunil <maleeshaindunil2003@gmail.com>
2025-08-10 17:41:48 +0530

    refactor: update .gitignore and improve firebase-admin modify  metadata

--- backend/src/firebase-admin.ts
+++ backend/src/firebase-admin.ts
@@ -1,14 +1,7 @@
-// src/firebase-admin.ts
 import * as admin from 'firebase-admin';
 
-// Replace this with your actual service account credentials.
-// The easiest way is to set a GOOGLE_APPLICATION_CREDENTIALS environment variable
-// pointing to your JSON key file.
-// For testing, you can directly initialize like this:
 const serviceAccount = require('../config/serviceAccountKey.json');
 
-// NOTE: You must replace the path above with the correct path to your Firebase service account key.
-// It's a file you download from the Firebase console.
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
