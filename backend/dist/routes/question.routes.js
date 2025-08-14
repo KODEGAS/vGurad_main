da2115a5f7f030b2ae7cec5872f9176b8e6185cf
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2025-08-14 11:36:53 +0000

    Deleted Tests

--- backend/dist/routes/question.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const question_controller_1 = require("../controllers/question.controller");
-const router = (0, express_1.Router)();
-router.get('/', question_controller_1.getQuestions);
-router.post('/', question_controller_1.createQuestion);
-exports.default = router;
