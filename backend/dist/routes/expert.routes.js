b60a980964dd3b3f622ed6c9839ebd5e49401460
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2025-08-14 11:36:53 +0000

    Deleted Tests

--- backend/dist/routes/expert.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const expert_controller_1 = require("../controllers/expert.controller");
-const router = (0, express_1.Router)();
-router.get('/', expert_controller_1.getExperts);
-exports.default = router;
